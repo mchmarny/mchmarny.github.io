1859fd8ae67841759c8f1e08ca1ae0d7f303b490
mchmarny <mchmarny@users.noreply.github.com>
2022-01-02 23:47:14 +0000

    cleanup mchmarny/blog@0e60da6d1cf8f0021edb62f73969c6a554e2e6d9

--- js/search.min.52b0061f5a72cae7c93cca169782e5ce1a32c5e2634913107d5be63e8ab14b3a.js
+++ js/search.min.52b0061f5a72cae7c93cca169782e5ce1a32c5e2634913107d5be63e8ab14b3a.js
@@ -1,4 +1,4 @@
-window.addEventListener("load",function(){var a=document.getElementById('search-box');if(typeof a!='undefined'&&a!=null){const c=algoliasearch('AUAFKENXPK','61824d075cbd6c54986adbb9dcf965e9'),b=instantsearch({indexName:'blog',searchClient:c,routing:!1});b.addWidgets([instantsearch.widgets.configure({hitsPerPage:10}),instantsearch.widgets.searchBox({container:a,placeholder:'Search posts'}),instantsearch.widgets.hits({container:'#hits',templates:{item(a){return console.log(a),`
+window.addEventListener("load",function(){var a=document.getElementById('search-box');if(typeof a!='undefined'&&a!=null){const c=algoliasearch('AUAFKENXPK','61824d075cbd6c54986adbb9dcf965e9'),b=instantsearch({indexName:'blog',searchClient:c,routing:!1});b.addWidgets([instantsearch.widgets.configure({hitsPerPage:10}),instantsearch.widgets.searchBox({container:a,placeholder:'Search posts'}),instantsearch.widgets.hits({container:'#hits',templates:{item(a){return`
                         <div>
                             <div class="hit-title">
                                 <a href="${a.relpermalink}" class="hit-url">
